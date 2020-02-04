3bb645c0243d33482e91cd6c53edc57a1e83424d
Rushikesh Deshkar <rushikesh.deshkar@transfast.com>
2020-02-04 20:55:05 +0530

    attribute function.

--- AzureFunctionApp/AzureFunctionApp/Program.cs
+++ AzureFunctionApp/AzureFunctionApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,29 @@ using System.Threading.Tasks;
 
 namespace AzureFunctionApp
 {
-    class Program
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //    }
+    //}
+
+    public static class QueueFunctions
     {
-        static void Main(string[] args)
+        //[FunctionName("QueueTrigger")]
+        //public static void QueueTrigger(
+        //    [QueueTrigger("myqueue-items")] string myQueueItem,
+        //    ILogger log)
+        //{
+        //    log.LogInformation($"C# function processed: {myQueueItem}");
+        //}
+
+        [FunctionName("QueueTrigger")]
+        public static void Run(
+    [QueueTrigger("requestqueue")] string myQueueItem,
+    ILogger log)
         {
+            log.LogInformation($"C# function processed: {myQueueItem}");
         }
     }
 }
